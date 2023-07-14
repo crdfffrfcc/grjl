8f352d99362c3eab6d1736ab8b216fd90cbd22a5
冯才民 <3230426339@qq.com>
2023-07-14 20:00:36 +0800

    7-14

--- resource/index.js
+++ resource/index.js
@@ -1,19 +1,29 @@
+let isfy = false
+let isanimation = true
+
+let surface = document.querySelector('.animation_surface')
+let right_Side = document.querySelector('.animation_rightSide')
+let side_right = document.querySelector('.animation_sideRight')
+
 let resize = function () {
+    let _ = document.querySelector('.container.bottom')
     if (document.documentElement.clientWidth > 1000) {
         document.body.style.padding = '50px'
         document.querySelector('.box').style.margin = '50px'
-        let _ = document.querySelector('.container.bottom')
         _.style.top = 0
         _.style.left = 0
-        delete _
     } else {
+        if (isfy) {
+            surface.style.transform = 'rotateY(0deg)'
+            right_Side.style.transform = 'rotateY(0deg)'
+            side_right.style.transform = 'rotateY(0deg)'
+        }
         document.body.style.padding = '0px'
         document.querySelector('.box').style.margin = '0px'
-        let _ = document.querySelector('.container.bottom')
         _.style.top = document.querySelector('.surface>.left_Side').clientHeight + document.querySelector('.surface>.right_Side').clientHeight + document.querySelector('.bottom>.left_Side').clientHeight + document.querySelector('.bottom>.right_Side').clientHeight + 'px'
         _.style.left = 0
-        delete _
     }
+    delete _
 }
 resize()
 window.addEventListener('resize', resize)
@@ -23,12 +33,6 @@ link.type = "text/css"
 link.href = "./resource/media.css"
 document.querySelector('head').appendChild(link)
 
-
-let isfy = false
-let isanimation = true
-let surface = document.querySelector('.animation_surface')
-let right_Side = document.querySelector('.animation_rightSide')
-let side_right = document.querySelector('.animation_sideRight')
 side_right.addEventListener('mouseover', () => {
     if (isanimation) {
         if (isfy) {
